9551af48f954da9a86f631bd8827fb19dff422dc
Fatimata <dramefatim75@gmail.com>
2024-06-17 12:20:12 +0200

    first commit

---
+++ src/test/java/com/wildcodeschool/Quests/QuestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.Quests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
